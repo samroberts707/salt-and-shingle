d3b9431e2f5ddba9a579d1e35cd1a7aecda20bf1
Sam Roberts <samroberts707@gmail.com>
2019-10-17 01:38:00 +0100

    Start initial version of Vue project

---
+++ src/components/Navigation.vue
@@ -0,0 +1,10 @@
+<template>
+    <nav class="navigation">
+        <router-link to="/">Home</router-link>
+    </nav>
+</template>
+<style lang="scss" scoped>
+    div.navigation {
+        display: block;
+    }
+</style>
