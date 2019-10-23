133980f6331b3dbc224a7a95290a3be97b311b34
Sam Roberts <samroberts707@gmail.com>
2019-10-23 13:40:12 +0100

    Start working on implementing the Obj renderings

--- src/components/Patron-Item.vue
+++ src/components/Patron-Item.vue
@@ -1,66 +1,70 @@
 <template>
-    <router-link :to="character.name" class="patron">
+    <div class="patron">
         <div class="card">
             <div class="canvas-wrapper" :id="character.name + '_canvas_wrapper'"></div>
             <h2>{{ character.name }}</h2>
         </div>
-    </router-link>
+    </div>
 </template>
 <script>
 var THREE = require('three');
+var OBJLoader = require('three-obj-loader');
 export default {
     props: ['character'],
     mounted() {
+        OBJLoader(THREE);
         var wrapper = document.getElementById(this.$props.character.name + '_canvas_wrapper');
         var scene = new THREE.Scene();
-        var camera = new THREE.PerspectiveCamera( 75, wrapper.offsetWidth/wrapper.offsetWidth, 0.1, 1000 );
-        var loader = new THREE.ObjectLoader();
+        var camera = new THREE.PerspectiveCamera( 75, 190/185, 0.1, 1000 );
+        var loader = new THREE.OBJLoader();
 
         
         var renderer = new THREE.WebGLRenderer({ alpha: true });
-        renderer.setSize( wrapper.offsetWidth, wrapper.offsetWidth );
+        renderer.setSize( 190, 185 );
         renderer.antialias = true;
         wrapper.appendChild(renderer.domElement);
 
-        var geometry = new THREE.BoxGeometry( 1, 1, 1 );
         var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-        var cube = new THREE.Mesh( geometry, material );
-        scene.add( cube );
 
-        // loader.load(
-        //     // this.$props.character.stl,
-        //     'https://raw.githubusercontent.com/mrdoob/three.js/master/examples/models/obj/male02/male02.obj',
-        //     function( object ) {
-        //         scene.add(object)
-        //     },
-        //     function( xhr ) {
-        //         console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-        //     },
-        //     function ( error ) {
-        //         console.log( 'An error happened' );
-        //     }
-        // );
+        // Basic Cube
+        // var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+        // var cube = new THREE.Mesh( geometry, material );
+        // scene.add( cube );
+
+        loader.load(
+            // this.$props.character.stl,
+            'https://raw.githubusercontent.com/samroberts707/salt-and-shingle/master/src/assets/character-objs/ellie.obj',
+            // 'assets/character-objs/captain-america-bust.stl',
+            function( object ) {
+                scene.add(object)
+            },
+            function( xhr ) {
+                console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+            },
+            function ( error ) {
+                console.log( error );
+            }
+        );
 
         camera.position.z = 5;
         // camera.position.y = 1;
 
         var animate = function () {
-            requestAnimationFrame( animate );
+            requestAnimationFrame(animate)
 
             // cube.rotation.x += 0.01;
-            cube.rotation.y += 0.01;
+            // cube.rotation.y += 0.01;
 
-            renderer.render( scene, camera );
-        };
+            renderer.render(scene, camera)
+        }
 
-        animate();
+        animate()
     }
 }
 </script>
 <style lang="scss" scoped>
-    a.patron {
+    .patron {
         display: block;
-        
         text-decoration: none;
         div.card {
             background-image: url('../assets/imgs/card.png');
@@ -73,24 +77,19 @@ export default {
             margin-right: auto;
             div.canvas-wrapper {
                 width: 190px;
-                height: 200px;
+                height: 185px;
                 margin-left: auto;
                 margin-right: auto;
                 padding-top: 30px;
-                margin-bottom: 15px;
+                margin-bottom: 10px;
             }
             h2 {
                 display: block;
                 text-align: center;
                 text-decoration: none;
                 color: #000;
-                font-size: 2vw;
-                @media screen and (max-width: 768px) {
-                    font-size: 5vw;
-                }
+                font-size: 29px;
             }
         }
-        
     }
-    
 </style>
